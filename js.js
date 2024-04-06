1a5ca525c0130cb9fff78684e0517121002ffaa1
arifrahmadi23 <muhammad231arif@gmail.com>
2024-04-07 00:49:11 +0700

    First Commit

---
+++ js.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Ini video Metallica");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("Ini audio Metallica");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("Ini gambar Metallica");
+});
